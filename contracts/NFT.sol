3bc8e9afe1b48a82a95b5afeb0c4a5eacf4b9347
Bradley <bradley.t.sheehan@gmail.com>
2022-10-05 19:32:51 -0600

    base

---
+++ contracts/NFT.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+// Amended by Red Velvet
+
+// import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/utils/Strings.sol";
+// import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Address.sol";
+// import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+// import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+// import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+// import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+pragma solidity ^0.8.0;
+
+contract NFT_ERC721 is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenSupply;
+
+    using Strings for uint256;
+
+    string baseURI = "ipfs://QmUaZRENUcQV9Uq8kAy4JCMQTi6x6DLQxLb4JT8oTzhwim/";
+    string public baseExtension = ".json";
+    uint256 public cost = 0.05 ether;
+    uint256 public maxSupply = 20;
+    uint256 public maxMintAmount = 3;
+    bool public paused = true;
+    address payable public payments;
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // public
+    function mint(uint256 _mintAmount) public payable {
+        uint256 supply = _tokenSupply.current();
+        require(!paused, "Must not be paused");
+        require(
+            _mintAmount < maxMintAmount + 1,
+            "Must mint less than max amount"
+        );
+        require(supply + _mintAmount <= maxSupply, "Must mint within supply");
+
+        require(
+            msg.value >= cost * _mintAmount,
+            "Must pay appropriate cost for NFT"
+        );
+
+        for (uint256 i = 0; i < _mintAmount; i++) {
+            _tokenSupply.increment();
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenSupply.current();
+    }
+}
