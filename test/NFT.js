d1732be5a212d143a0610f8df819ea5fbc262c3e
Bradley <bradley.t.sheehan@gmail.com>
2022-10-05 23:45:24 -0600

    clear

--- test/NFT.js
+++ test/NFT.js
@@ -6,348 +6,62 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe('NFT', function () {
-  let NFT, nftContract, owner, addr1, addr2, addr3, addrs
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+describe("NFT contract", function () {
+  let NFT;
+  let nftContract;
+  let _name = "Hack3rsClub";
+  let _symbol = "HC";
+  let account1, otheraccounts;
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
   beforeEach(async function () {
-    NFT = await ethers.getContractFactory('NFT')
-    ;[owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners()
-    nftContract = await NFT.deploy()
-  })
+    NFT = await ethers.getContractFactory("NFT");
+    [owner, account1, ...otheraccounts] = await ethers.getSigners();
 
-  describe('Deployment', function () {
-    it('Should set the right owner', async function () {
-      expect(await nftContract.owner()).to.equal(owner.address)
-    })
-  })
+    nftContract = await NFT.deploy(_name, _symbol);
+  });
 
-  describe('setIsAllowListActive', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).setIsAllowListActive(true),
-      ).to.be.revertedWith('Ownable: caller is not the owner')
-    })
+  // You can nest describe calls to create subsections.
+  describe("Deployment", function () {
+    it("Should has the correct name and symbol ", async function () {
+      expect(await nftContract.name()).to.equal(_name);
+      expect(await nftContract.symbol()).to.equal(_symbol);
+    });
 
-    it('Should should set isAllowListActive by owner', async function () {
-      const expectedValue = true
+    it("Should start with supply of 0", async function () {
+      expect(await nftContract.totalSupply()).to.equal(0);
+    });
 
-      await nftContract.connect(owner).setIsAllowListActive(expectedValue)
+    it("Should fail to mint a token with token ID 1 to account1", async function () {
+      const address1 = account1.address;
+      await nftContract.connect(address1).mint(1, {
+        value: ethers.utils.parseEther("1.0"),
+      });
+      expect(await nftContract.balanceOf(address1)).to.equal(0);
+    });
 
-      expect(await nftContract.isAllowListActive()).to.equal(expectedValue)
-    })
-  })
+    it("Should unpause contract", async function () {
+      await nftContract.pause(false);
+      expect(await nftContract.paused()).to.equal(false);
+    });
 
-  describe('setAllowList', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).setAllowList([addr1.address], 10),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-
-    // THIS IS NOT WORKING, BECAUSE _allowList is private
-    // it('Should should set _allowList by owner', async function () {
-    //   const expectedValue = 10
-
-    //   await nftContract
-    //     .connect(owner)
-    //     .setAllowList([addr1.address, addr2.address], expectedValue)
-
-    //   expect(
-    //     await nftContract.connect(owner)._allowList(addr1.address),
-    //   ).to.equal(expectedValue)
-    //   expect(
-    //     await nftContract.connect(owner)._allowList(addr2.address),
-    //   ).to.equal(expectedValue)
-    // })
-
-    it('Should should set _allowList by owner', async function () {
-      const expectedValue = 10
+    it("Should mint a token with token ID 1 & 2 to account1", async function () {
+      const address1 = account1.address;
+      await nftContract
+        .connect(address1)
+        .mint({ value: ethers.utils.parseEther("0.1") });
+      expect(await nftContract.ownerOf(1)).to.equal(address1);
 
       await nftContract
-        .connect(owner)
-        .setAllowList([addr1.address, addr2.address], expectedValue)
+        .connect(address1)
+        .mint({ value: ethers.utils.parseEther("0.1") });
+      expect(await nftContract.ownerOf(2)).to.equal(address1);
 
-      expect(await nftContract.numAvailableToMint(addr1.address)).to.equal(
-        expectedValue,
-      )
-      expect(await nftContract.numAvailableToMint(addr2.address)).to.equal(
-        expectedValue,
-      )
-    })
-
-    it('Should should set _allowList by owner', async function () {
-      await nftContract.connect(owner).setIsAllowListActive(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await expect(
-        nftContract.connect(addr1).mintAllowList(1, overrides),
-      ).to.be.revertedWith('Exceeded max available to purchase')
-
-      await nftContract.connect(owner).setAllowList([addr1.address], 1)
-      await nftContract.connect(addr1).mintAllowList(1, overrides)
-
-      //assert
-      expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-      await expect(nftContract.ownerOf(1)).to.be.revertedWith(
-        'URI query for nonexistent token',
-      )
-    })
-  })
-
-  describe('numAvailableToMint', function () {
-    it('Should show numAvailableToMint', async function () {
-      const expectedValue = 5
-
-      await nftContract
-        .connect(owner)
-        .setAllowList([addr1.address, addr2.address], expectedValue)
-
-      expect(await nftContract.numAvailableToMint(addr1.address)).to.equal(
-        expectedValue,
-      )
-      expect(await nftContract.numAvailableToMint(addr2.address)).to.equal(
-        expectedValue,
-      )
-      expect(await nftContract.numAvailableToMint(addr3.address)).to.equal(
-        0,
-      )
-    })
-  })
-
-  describe('mintAllowList', function () {
-    it('Should be reverted because the isAllowListActive is false', async function () {
-      await nftContract.connect(owner).setIsAllowListActive(false)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(owner).setAllowList([addr1.address], 1)
-      await expect(
-        nftContract.connect(addr1).mintAllowList(1, overrides),
-      ).to.be.revertedWith('Allow list is not active')
-    })
-
-    it('Should be reverted if exceeded max available to purchase', async function () {
-      await nftContract.connect(owner).setIsAllowListActive(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.246'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(owner).setAllowList([addr1.address], 1)
-      await expect(
-        nftContract.connect(addr1).mintAllowList(2, overrides),
-      ).to.be.revertedWith('Exceeded max available to purchase')
-    })
-
-    it('Should be reverted because the caller exceeds max token', async function () {
-      await nftContract.connect(owner).setIsAllowListActive(true)
-      const overrides = {
-        value: ethers.utils.parseEther('24.6'), // ether in this case MUST be a string
-      }
-      //50*200 = 10000
-      for (let i = 0; i < 50; i++) {
-        await nftContract
-          .connect(owner)
-          .setAllowList([addrs[i].address], 200)
-        await nftContract.connect(addrs[i]).mintAllowList(200, overrides)
-      }
-      await nftContract
-        .connect(owner)
-        .setAllowList([addrs[50].address], 200)
-      await expect(
-        nftContract.connect(addrs[50]).mintAllowList(1, overrides),
-      ).to.be.revertedWith('Purchase would exceed max tokens')
-    })
-
-    it('Should be reverted because the caller do not have enough fund', async function () {
-      await nftContract.connect(owner).setIsAllowListActive(true)
-
-      const overrides = {
-        value: ethers.utils.parseEther('0.122'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(owner).setAllowList([addr1.address], 1)
-      await expect(
-        nftContract.connect(addr1).mintAllowList(1, overrides),
-      ).to.be.revertedWith('Ether value sent is not correct')
-    })
-
-    it('Should mint token', async function () {
-      const baseurl = 'ipfs://test.url/'
-      nftContract.connect(owner).setBaseURI(baseurl)
-      await nftContract.connect(owner).setIsAllowListActive(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(owner).setAllowList([addr1.address], 1)
-      await nftContract.connect(addr1).mintAllowList(1, overrides)
-
-      expect(await nftContract.tokenURI(0)).to.equal(baseurl + '0') //ipfs://test.url/0
-      expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-    })
-  })
-
-  describe('setBaseURI', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).setBaseURI('url'),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-
-    it('Should set the baseTokenURI by owner', async function () {
-      const baseurl = 'ipfs://test.url/'
-      nftContract.connect(owner).setBaseURI(baseurl)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(owner).setSaleState(true)
-      await nftContract.connect(addr1).mint(1, overrides)
-
-      expect(await nftContract.tokenURI(0)).to.equal(baseurl + '0')
-      expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-    })
-  })
-
-  describe('setProvenance', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).setProvenance('random hash'),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-
-    it('Should should set PROVENANCE by owner', async function () {
-      const expectedValue = 'random hash'
-
-      await nftContract.connect(owner).setProvenance(expectedValue)
-
-      expect(await nftContract.PROVENANCE()).to.equal(expectedValue)
-    })
-  })
-
-  describe('reserve', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).reserve(1),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-
-    it('Should reserve tokens by owner', async function () {
-      const baseurl = 'ipfs://test.url/'
-      nftContract.connect(owner).setBaseURI(baseurl)
-      await nftContract.connect(owner).reserve(5)
-      for (let i = 0; i < 5; i++) {
-        expect(await nftContract.tokenURI(i)).to.equal(baseurl + i)
-      }
-    })
-  })
-
-  describe('setSaleState', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).setSaleState(true),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-
-    it('Should should set saleIsActive by owner', async function () {
-      const expectedValue = true
-
-      await nftContract.connect(owner).setSaleState(expectedValue)
-
-      expect(await nftContract.saleIsActive()).to.equal(expectedValue)
-    })
-  })
-
-  describe('mint', function () {
-    it('Should be reverted because the saleIsActive is false', async function () {
-      await nftContract.connect(owner).setSaleState(false)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await expect(
-        nftContract.connect(addr1).mint(1, overrides),
-      ).to.be.revertedWith('Sale must be active to mint tokens')
-    })
-
-    it('Should be reverted if exceeded max token purchase', async function () {
-      await nftContract.connect(owner).setSaleState(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.738'), // ether in this case MUST be a string
-      }
-
-      await expect(
-        nftContract.connect(addr1).mint(6, overrides),
-      ).to.be.revertedWith('Exceeded max token purchase')
-    })
-
-    it('Should be reverted because the caller exceeds max token', async function () {
-      await nftContract.connect(owner).setSaleState(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.615'), // ether in this case MUST be a string
-      }
-
-      //5 token each time * 2000 = 10000
-      for (let i = 0; i < 2000; i++) {
-        await nftContract.connect(addr1).mint(5, overrides)
-      }
-
-      await expect(
-        nftContract.connect(addr1).mint(1, overrides),
-      ).to.be.revertedWith('Purchase would exceed max tokens')
-    })
-
-    it('Should be reverted because the caller do not have enough fund', async function () {
-      await nftContract.connect(owner).setSaleState(true)
-
-      const overrides = {
-        value: ethers.utils.parseEther('0.122'), // ether in this case MUST be a string
-      }
-      await expect(
-        nftContract.connect(addr1).mint(1, overrides),
-      ).to.be.revertedWith('Ether value sent is not correct')
-    })
-
-    it('Should mint token', async function () {
-      const baseurl = 'ipfs://test.url/'
-      nftContract.connect(owner).setBaseURI(baseurl)
-      await nftContract.connect(owner).setSaleState(true)
-      const overrides = {
-        value: ethers.utils.parseEther('0.123'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(addr1).mint(1, overrides)
-
-      expect(await nftContract.tokenURI(0)).to.equal(baseurl + '0')
-      expect(await nftContract.ownerOf(0)).to.equal(addr1.address)
-    })
-  })
-
-  describe('withdraw', function () {
-    it('Should be reverted because the caller is not owner', async function () {
-      await expect(
-        nftContract.connect(addr1).withdraw(),
-      ).to.be.revertedWith('caller is not the owner')
-    })
-    it('Should withdraw fund by the owner', async function () {
-      await nftContract.connect(owner).withdraw()
-    })
-
-    it('Should withdraw fund by the owner', async function () {
-      await nftContract.connect(owner).setSaleState(true)
-      const overrides = {
-        value: ethers.utils.parseEther('5'), // ether in this case MUST be a string
-      }
-      await nftContract.connect(addr1).mint(1, overrides)
-      const accountBalanceBeforeWithdraw = ethers.utils.formatEther(
-        await nftContract.provider.getBalance(owner.address),
-      )
-
-      await nftContract.connect(owner).withdraw()
-      const accountBalanceAfterWithdraw = ethers.utils.formatEther(
-        await nftContract.provider.getBalance(owner.address),
-      )
-
-      expect(
-        parseInt(accountBalanceAfterWithdraw) >
-          parseInt(accountBalanceBeforeWithdraw),
-      ).to.be.true
-
-      //get smart contract balance before withdraw and smart contract balance after withdraw
-    })
-  })
-})
+      expect(await nftContract.balanceOf(address1)).to.equal(2);
+    });
+  });
+});
